7a88a829d754f26b24ffa36a3a7dc5a12d6eac95
sojib-111 <sojibislam887799@gmail.com>
2025-12-04 08:53:33 +0600

    Update kick.js

--- scripts/cmds/kick.js
+++ scripts/cmds/kick.js
@@ -1,47 +1,60 @@
-const DIG = require("discord-image-generation");
-const fs = require("fs-extra");
-
-
 module.exports = {
-		config: {
-				name: "kiss",
-				aliases: ["kiss"],
-				version: "1.0",
-				author: "NIB",
-				countDown: 5,
-				role: 0,
-				shortDescription: "KISS",
-				longDescription: "",
-				category: "funny",
-				guide: "{pn}"
+	config: {
+		name: "kick",
+		version: "1.2",
+		author: "Ｓｏｊｉｂ ◉‿◉",
+		countDown: 5,
+		role: 1,
+		shortDescription: {
+			vi: "Kick thành viên",
+			en: "Kick member"
 		},
+		longDescription: {
+			vi: "Kick thành viên khỏi box chat",
+			en: "Kick member out of chat box"
+		},
+		category: "box chat",
+		guide: {
+			vi: "   {pn} @tags: dùng để kick những người được tag",
+			en: "   {pn} @tags: use to kick members who are tagged"
+		}
+	},
 
+	langs: {
+		vi: {
+			needAdmin: "Vui lòng thêm quản trị viên cho bot trước khi sử dụng tính năng này"
+		},
+		en: {
+			needAdmin: "Please add admin for bot before using this feature"
+		}
+	},
 
-
-		onStart: async function ({ api, message, event, args, usersData }) {
-			let one, two;
-				const mention = Object.keys(event.mentions);
-			if(mention.length == 0) return message.reply("Please mention someone");
-else if(mention.length == 1){
- one = event.senderID
-	 two = mention[0];
-
-} else{
- one = mention[1]
-	 two = mention[0];
-
-}
-
-
-				const avatarURL1 = await usersData.getAvatarUrl(two);
-		const avatarURL2 = await usersData.getAvatarUrl(one);
-		const img = await new DIG.Kiss().getImage(avatarURL1, avatarURL2);
-		const pathSave = `${__dirname}/tmp/${one}_${two}kiss.png`;
-		fs.writeFileSync(pathSave, Buffer.from(img));
-		const content = "😘😘"
-		message.reply({
-			body: `${(content || "Bópppp 😵‍💫😵")}`,
-			attachment: fs.createReadStream(pathSave)
-		}, () => fs.unlinkSync(pathSave));
+	onStart: async function ({ message, event, args, threadsData, api, getLang }) {
+		const adminIDs = await threadsData.get(event.threadID, "adminIDs");
+		if (!adminIDs.includes(api.getCurrentUserID()))
+			return message.reply(getLang("needAdmin"));
+		async function kickAndCheckError(uid) {
+			try {
+				await api.removeUserFromGroup(uid, event.threadID);
+			}
+			catch (e) {
+				message.reply(getLang("needAdmin"));
+				return "ERROR";
+			}
+		}
+		if (!args[0]) {
+			if (!event.messageReply)
+				return message.SyntaxError();
+			await kickAndCheckError(event.messageReply.senderID);
+		}
+		else {
+			const uids = Object.keys(event.mentions);
+			if (uids.length === 0)
+				return message.SyntaxError();
+			if (await kickAndCheckError(uids.shift()) === "ERROR")
+				return;
+			for (const uid of uids)
+				api.removeUserFromGroup(uid, event.threadID);
+		}
 	}
 };
