3a7854c0dd6788b2909057c10b4fe34a5e51c93a
sojib-111 <sojibislam887799@gmail.com>
2025-12-30 12:19:27 +0600

    Update kiss2.js

--- scripts/cmds/kiss2.js
+++ scripts/cmds/kiss2.js
@@ -1,47 +1,77 @@
-const DIG = require("discord-image-generation");
-const fs = require("fs-extra");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
+const mahmud = async () => {
+  const base = await axios.get(
+    "https://raw.githubusercontent.com/mahmudx7/HINATA/main/baseApiUrl.json"
+  );
+  return base.data.mahmud;
+};
+
+/**
+ * @author MahMUD
+ * @author: do not delete it
+ */
 
 module.exports = {
-		config: {
-				name: "kiss",
-				aliases: ["kiss"],
-				version: "1.0",
-				author: "NIB",
-				countDown: 5,
-				role: 0,
-				shortDescription: "KISS",
-				longDescription: "",
-				category: "funny",
-				guide: "{pn}"
-		},
+  config: {
+    name: "kiss2",
+    version: "1.7",
+    author: "🅢🅞🅙🅘🅑",
+    countDown: 5,
+    role: 0,
+    longDescription: "Generate anime-style kiss image",
+    category: "love",
+    guide: "{pn} @mention"
+  },
 
+  onStart: async function ({ message, event, api }) {
+    try {
+      const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68);
+      if (module.exports.config.author.trim() !== obfuscatedAuthor) {
+        return api.sendMessage(
+          "❌ | You are not authorized to change the author name.",
+          event.threadID,
+          event.messageID
+        );
+      }
 
+      const mention = Object.keys(event.mentions);
+      if (mention.length === 0) {
+        return message.reply("Please mention someone to kiss 💋");
+      }
 
-		onStart: async function ({ api, message, event, args, usersData }) {
-			let one, two;
-				const mention = Object.keys(event.mentions);
-			if(mention.length == 0) return message.reply("Please mention someone");
-else if(mention.length == 1){
- one = event.senderID
-	 two = mention[0];
+      const senderID = event.senderID;
+      const targetID = mention[0];
 
-} else{
- one = mention[1]
-	 two = mention[0];
+      const base = await mahmud();
+      const apiURL = `${base}/api/kiss`;
 
-}
+      const response = await axios.post(
+        apiURL,
+        { senderID, targetID },
+        { responseType: "arraybuffer" }
+      );
 
+      const imgPath = path.join(
+        __dirname,
+        `kiss_${senderID}_${targetID}.png`
+      );
+      fs.writeFileSync(imgPath, Buffer.from(response.data, "binary"));
 
-				const avatarURL1 = await usersData.getAvatarUrl(two);
-		const avatarURL2 = await usersData.getAvatarUrl(one);
-		const img = await new DIG.Kiss().getImage(avatarURL1, avatarURL2);
-		const pathSave = `${__dirname}/tmp/${one}_${two}kiss.png`;
-		fs.writeFileSync(pathSave, Buffer.from(img));
-		const content = "😘😘"
-		message.reply({
-			body: `${(content || "Bópppp 😵‍💫😵")}`,
-			attachment: fs.createReadStream(pathSave)
-		}, () => fs.unlinkSync(pathSave));
-	}
+      message.reply({
+        body: "💋 Here’s your kiss image!",
+        attachment: fs.createReadStream(imgPath)
+      });
+
+      setTimeout(() => {
+        if (fs.existsSync(imgPath)) fs.unlinkSync(imgPath);
+      }, 10000);
+
+    } catch (err) {
+      console.error("Error in kiss command:", err.message || err);
+      message.reply("🥹 error, contact MahMUD.");
+    }
+  }
 };
